5f6cb3b2292bdc5f82f5e0fbc8744c28a2b11115
Glenwin Bernabe <glenwinbernabe@gmail.com>
2025-11-29 22:28:46 +0800

    fixed icons

--- header.php
+++ header.php
@@ -24,7 +24,10 @@ require_once 'config.php';
     
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="logo/logo.ico">
-    <link rel="icon" type="image/png" href="logo/logo-192x192.png">
+    <link rel="shortcut icon" href="logo/logo-192x192.png">
+    
+    <!-- Prevent favicon.ico request -->
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👑</text></svg>">
     
     <!-- Styles and Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
